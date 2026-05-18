650ff6644bafcc14034343e6870f63d56f5c1bc7
lilKriT <lilKriTwebDev@gmail.com>
2026-05-18 23:38:50 +0200

    Added new files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Rubik } from "next/font/google";
 import "./globals.css";
+
+const rubikSans = Rubik({
+  variable: "--font-rubik-sans",
+  subsets: ["latin"],
+  style: ["normal"],
+  weight: ["300", "400", "500"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
