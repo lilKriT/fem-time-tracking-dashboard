da186b0074f529763675b52c0293b22633024dbc
lilKriT <lilKriTwebDev@gmail.com>
2026-06-08 22:55:19 +0200

    Improving.

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,14 +1,17 @@
 interface CardProps {
   title: string;
   color: string;
-  icon?: React.ReactNode;
+  icon: React.ReactNode;
 }
 
-const Card = ({ title }: CardProps) => {
+const Card = ({ title, color, icon }: CardProps) => {
   return (
-    <article>
+    <article
+      className={`${color} ${icon} bg-no-repeat bg-position-[right_1rem_top_-0.5rem]`}
+    >
       <div>
         <h2>{title}</h2>
+        <h3>{color}</h3>
       </div>
     </article>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import { data } from "./data";
 import Card from "./_components/Card";
 
 const ActivityMeta = {
-  Work: { color: "", icon: "" },
-  Play: { color: "", icon: "" },
-  Study: { color: "", icon: "" },
-  Exercise: { color: "", icon: "" },
-  Social: { color: "", icon: "" },
-  "Self Care": { color: "", icon: "" },
+  Work: { color: "bg-orange-300", icon: "bg-[url('/icon-work.svg')]" },
+  Play: { color: "bg-blue-300", icon: "bg-[url('/icon-play.svg')]" },
+  Study: { color: "bg-pink-400", icon: "bg-[url('/icon-study.svg')]" },
+  Exercise: { color: "bg-green-400", icon: "bg-[url('/icon-exercise.svg')]" },
+  Social: { color: "bg-purple-700", icon: "bg-[url('/icon-social.svg')]" },
+  "Self Care": {
+    color: "bg-yellow-300",
+    icon: "bg-[url('/icon-self-care.svg')]",
+  },
 };
 
 const getActivityMeta = (title: string) => {
@@ -41,7 +44,11 @@ export default function Home() {
           </div>
 
           {/* Timeframe settings: */}
-          <div>bla bla bla bla</div>
+          <div className="flex flex-col justify-center items-start px-6 py-6 gap-4">
+            <button>Daily</button>
+            <button>Weekly</button>
+            <button>Monthly</button>
+          </div>
         </div>
 
         {/* Cards */}
@@ -50,6 +57,7 @@ export default function Home() {
             key={activity.title}
             title={activity.title}
             color={getActivityMeta(activity.title).color}
+            icon={getActivityMeta(activity.title).icon}
           />
         ))}
       </div>
