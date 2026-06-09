055bc359486e8befb888bba2aee6d98cb667c456
lilKriT <lilKriTwebDev@gmail.com>
2026-06-09 02:42:14 +0200

    Displaying data.

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,23 +1,39 @@
+import { ITimeframe } from "../data";
+
 interface CardProps {
   title: string;
   color: string;
   icon: React.ReactNode;
+  mode: "daily" | "weekly" | "monthly";
+  timeframes: {
+    daily: ITimeframe;
+    weekly: ITimeframe;
+    monthly: ITimeframe;
+  };
 }
 
-const Card = ({ title, color, icon }: CardProps) => {
+const Card = ({ title, color, icon, mode, timeframes }: CardProps) => {
+  const timeframe = timeframes[mode];
+
   return (
     <article
       className={`${color} ${icon} bg-no-repeat bg-position-[right_1rem_top_-0.5rem] rounded-2xl flex text-purple-50`}
     >
       {/* The inner frame */}
-      <div className="bg-navy-900 mt-12 rounded-2xl w-full p-8">
-        <h2 className="font-medium text-lg">{title}</h2>
+      <div className="bg-navy-900 mt-12 rounded-2xl w-full p-8 flex flex-col gap-2">
+        <div className="flex justify-between items-center">
+          <h2 className="font-medium text-lg">{title}</h2>
 
-        {/* Apparently this is better then "Image"... */}
-        <img src={"/icon-ellipsis.svg"} />
+          {/* Apparently this is better then "Image"... */}
+          <img src={"/icon-ellipsis.svg"} className="cursor-pointer" />
+        </div>
 
-        <p className="text-6xl font-light">32hrs</p>
-        <p className="font-medium text-navy-200">Last Week - 36hrs</p>
+        <p className="text-6xl font-light mt-4">{timeframe.current}hrs</p>
+        {/* Different data depending on the mode */}
+        <p className="font-medium text-navy-200">
+          {mode === "daily" && "Last Day"} {mode === "weekly" && "Last Week"}
+          {mode === "monthly" && "Last Month"} - {timeframe.previous}hrs{" "}
+        </p>
       </div>
     </article>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { data } from "./data";
 import Card from "./_components/Card";
+import { useState } from "react";
 
 const ActivityMeta = {
   Work: { color: "bg-orange-300", icon: "bg-[url('/icon-work.svg')]" },
@@ -27,12 +28,14 @@ const getActivityMeta = (title: string) => {
 };
 
 export default function Home() {
+  const [mode, setMode] = useState<"daily" | "weekly" | "monthly">("weekly");
+
   return (
     <section className="min-h-dvh flex justify-center items-center">
       <div className="grid grid-cols-4 grid-rows-2 gap-8 max-w-7xl">
         {/* First, double sized cell */}
-        <div className="row-span-2 bg-navy-900 rounded-2xl ">
-          <div className="bg-purple-600 rounded-2xl p-6 flex flex-col gap-2">
+        <div className="row-span-2 bg-navy-900 rounded-2xl flex flex-col">
+          <div className="bg-purple-600 rounded-2xl p-6 flex flex-col gap-2 grow">
             {/* User card */}
             <Image
               src={"/image-jeremy.png"}
@@ -41,19 +44,28 @@ export default function Home() {
               height={234}
               className="border-2 border-white rounded-full w-20 aspect-square"
             />
-            <p className="text-navy-200">Report for</p>
+            <p className="text-navy-200 mt-12 font-medium">Report for</p>
             <h2 className="text-5xl font-light">Jeremy Robson</h2>
           </div>
 
           {/* Timeframe settings: */}
           <div className="flex flex-col justify-center items-start px-6 py-6 gap-4">
-            <button className="btn" onClick={() => console.log("Daily")}>
+            <button
+              className={`btn ${mode === "daily" && "btn--active"}`}
+              onClick={() => setMode("daily")}
+            >
               Daily
             </button>
-            <button className="btn" onClick={() => console.log("Weekly")}>
+            <button
+              className={`btn ${mode === "weekly" && "btn--active"}`}
+              onClick={() => setMode("weekly")}
+            >
               Weekly
             </button>
-            <button className="btn" onClick={() => console.log("Monthly")}>
+            <button
+              className={`btn ${mode === "monthly" && "btn--active"}`}
+              onClick={() => setMode("monthly")}
+            >
               Monthly
             </button>
           </div>
@@ -66,6 +78,8 @@ export default function Home() {
             title={activity.title}
             color={getActivityMeta(activity.title).color}
             icon={getActivityMeta(activity.title).icon}
+            mode={mode}
+            timeframes={activity.timeframes}
           />
         ))}
       </div>
