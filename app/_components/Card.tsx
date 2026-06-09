6e75c2993f708b43bd7f987fa019625f447a71be
lilKriT <lilKriTwebDev@gmail.com>
2026-06-09 03:08:45 +0200

    Added active states.

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -20,7 +20,7 @@ const Card = ({ title, color, icon, mode, timeframes }: CardProps) => {
       className={`${color} ${icon} bg-no-repeat bg-position-[right_1rem_top_-0.5rem] rounded-2xl flex text-purple-50`}
     >
       {/* The inner frame */}
-      <div className="bg-navy-900 mt-12 rounded-2xl w-full p-8 flex flex-col gap-2">
+      <div className="bg-navy-900 hover:bg-[hsl(236,45%,33%)] motion-safe:duration-300 ease-in-out mt-12 rounded-2xl w-full p-8 flex flex-col gap-2">
         <div className="flex justify-between items-center">
           <h2 className="font-medium text-lg">{title}</h2>
 
@@ -32,7 +32,7 @@ const Card = ({ title, color, icon, mode, timeframes }: CardProps) => {
         {/* Different data depending on the mode */}
         <p className="font-medium text-navy-200">
           {mode === "daily" && "Last Day"} {mode === "weekly" && "Last Week"}
-          {mode === "monthly" && "Last Month"} - {timeframe.previous}hrs{" "}
+          {mode === "monthly" && "Last Month"} - {timeframe.previous}hrs
         </p>
       </div>
     </article>
