7de66fb1f79b39bf6e909d0eafb5912b0a66d5b0
lilKriT <lilKriTwebDev@gmail.com>
2026-06-09 20:15:17 +0200

    Completed.

--- app/page.tsx
+++ app/page.tsx
@@ -36,21 +36,27 @@ export default function Home() {
       <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-8 w-full max-w-7xl">
         {/* First, double sized cell */}
         <div className="row-span-2 bg-navy-900 rounded-2xl flex flex-col">
-          <div className="bg-purple-600 rounded-2xl p-6 flex flex-col gap-2 grow">
+          <div className="bg-purple-600 rounded-2xl p-6 flex items-center sm:flex-col sm:items-start gap-4 grow">
             {/* User card */}
             <Image
               src={"/image-jeremy.png"}
               alt="Face of Jeremy"
               width={234}
               height={234}
-              className="border-2 border-white rounded-full w-20 aspect-square"
+              className="border-2 border-white rounded-full w-14 sm:w-20 aspect-square"
             />
-            <p className="text-navy-200 mt-12 font-medium">Report for</p>
-            <h2 className="text-5xl font-light">Jeremy Robson</h2>
+            <div className=" sm:mt-12">
+              <p className="text-navy-200 font-medium text-sm sm:text-base">
+                Report for
+              </p>
+              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-light">
+                Jeremy Robson
+              </h2>
+            </div>
           </div>
 
           {/* Timeframe settings: */}
-          <div className="flex flex-col justify-center items-start px-6 py-6 gap-4">
+          <div className="flex justify-between items-center sm:flex-col sm:items-start px-6 py-6 gap-4">
             <button
               className={`btn ${mode === "daily" && "btn--active"}`}
               onClick={() => setMode("daily")}
